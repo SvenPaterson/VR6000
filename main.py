237f0b838d712d41df7c4722e981d07fd76bb846
SvenPaterson <stephenjgarden@icloud.com>
2023-03-01 10:38:42 -0500

    create code to plot VR-6000 profile export

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
-#import stl file and plot it
-from stl import mesh
+# export profile from VR-6000 software as DXF
+# import DXF file and plot it
 import matplotlib.pyplot as plt
-import numpy as np
-from mpl_toolkits import mplot3d
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
-from scipy.spatial import ConvexHull
-from scipy.spatial import Delaunay
-from scipy.spatial import Voronoi, voronoi_plot_2d
-from scipy.spatial import KDTree
-from scipy.spatial import distance
-from scipy.spatial import cKDTree
-from scipy.spatial import SphericalVoronoi
+import ezdxf
 
-# Load the STL files and add the vectors to the plot
-your_mesh = mesh.Mesh.from_file('test-scan.stl')
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
-# Auto scale to the mesh size
-scale = your_mesh.points.flatten('-1')
-ax.auto_scale_xyz(scale, scale, scale)
-# Show the plot to the screen
+# Load the DXF files and add the vectors to the plot
+doc = ezdxf.readfile('test-scan.dxf')
+msp = doc.modelspace()
+
+fig = plt.figure(figsize=(14, 5))
+ax = fig.add_subplot(111)
+
+max_x, min_x = 0, 0 # for setting plot limits
+for e in msp:
+    if e.dxftype() == 'LINE':
+        if e.dxf.start[0] > -5 and e.dxf.start[0] < 5:
+            # ignores all lines not part of seal
+            # i.e. the scanning fixture
+            continue
+        x = [e.dxf.start[0], e.dxf.end[0]]
+        y = [e.dxf.start[1], e.dxf.end[1]]
+        if x[1] > max_x:
+            max_x = x[1]
+        if x[0] < min_x:
+            min_x = x[0]
+        ax.plot(x, y, color='black')
+
+padding = 5
+ax.set_xlim((min_x - padding), (max_x + padding))
+
+plt.axis('scaled') # preserves scale of scan
+plt.tight_layout()
 plt.show()
 
