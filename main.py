e2f77bfbc25cb4956d67445f066e676f937f9dad
SvenPaterson <stephenjgarden@icloud.com>
2023-05-02 11:55:37 -0400

    switch to icp with cylinders instead of mesh

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from tqdm import tqdm
 from pathlib import Path
 from tkinter import filedialog, Tk
 from joblib import Parallel, delayed
+from sklearn.decomposition import PCA
+from trimesh import util
 
 def split_path(file_path):
     """
@@ -35,81 +37,92 @@ def load_stl(file_path):
         A Trimesh object.
     """
     mesh = trimesh.load_mesh(file_path)
-    mesh.vertices = mesh.vertices - mesh.centroid
+    mesh.vertices -= mesh.centroid
     return mesh
 
-def decimate_mesh_slow(mesh, fraction):
-    """
-    Decimate a mesh using a slower method by iteratively applying quadric decimation.
+def decimate_mesh(mesh, fraction, n_jobs=8, max_iterations=100):
+    def create_lightweight_mesh(vertices, faces):
+        return trimesh.Trimesh(vertices=vertices, faces=faces, process=False)
 
-    Args:
-        mesh (trimesh.Trimesh): The input mesh to be decimated.
-        fraction (float): The fraction of faces to keep in the decimated mesh (0 < fraction <= 1).
+    def simplify_quadric_decimation_chunk(faces_chunk):
+        mesh_copy = create_lightweight_mesh(mesh.vertices, faces_chunk)
+        return mesh_copy.simplify_quadric_decimation(int(faces_chunk.shape[0] * 0.9))
 
-    Returns:
-        trimesh.Trimesh: The decimated mesh.
-    """
     target_faces = int(mesh.faces.shape[0] * fraction)
 
     with tqdm(total=target_faces, desc="Decimating mesh", ncols=80) as pbar:
+        iteration = 0
         while mesh.faces.shape[0] > target_faces:
-            mesh = mesh.simplify_quadric_decimation(int(mesh.faces.shape[0] * 0.95))
-            pbar.update(int(mesh.faces.shape[0] * 0.05))
+            iteration += 1
+            if iteration > max_iterations:
+                break
 
-    return mesh
-
-def decimate_mesh(mesh, fraction, n_jobs=8):
-    """
-    Decimate a mesh using a faster method by parallelizing quadric decimation.
-
-    Args:
-        mesh (trimesh.Trimesh): The input mesh to be decimated.
-        fraction (float): The fraction of faces to keep in the decimated mesh (0 < fraction <= 1).
-        n_jobs (int, optional): The number of parallel jobs to run. Default is 8.
-
-    Returns:
-        trimesh.Trimesh: The decimated mesh.
-    """
-    target_faces = int(mesh.faces.shape[0] * fraction)
-
-    with tqdm(total=target_faces, desc="Decimating mesh", ncols=80) as pbar:
-        while mesh.faces.shape[0] > target_faces:
-            # Split the mesh faces into n_jobs chunks for parallel processing
             faces_chunks = np.array_split(mesh.faces, n_jobs)
-            
-            # Define the function to be parallelized
-            def simplify_quadric_decimation_chunk(faces_chunk):
-                mesh_copy = mesh.copy()
-                mesh_copy.faces = faces_chunk
-                return mesh_copy.simplify_quadric_decimation(int(faces_chunk.shape[0] * 0.95))
-            
-            # Apply the function to each faces chunk in parallel
+
             new_meshes = Parallel(n_jobs=n_jobs)(
-                delayed(simplify_quadric_decimation_chunk)(chunk) 
-                                                    for chunk in faces_chunks)
-            
-            # Combine the meshes
-            mesh = trimesh.util.concatenate(new_meshes)
+                delayed(simplify_quadric_decimation_chunk)(chunk)
+                for chunk in faces_chunks)
+
+            mesh = util.concatenate(new_meshes)
             pbar.update(int(mesh.faces.shape[0] - target_faces))
 
     return mesh
 
+def plot_meshes(meshes, colors=None, opacities=None, title=None):
+    # Create a Pyvista plotter
+    plotter = pv.Plotter()
+
+    # Set default colors and opacities if not provided
+    if colors is None:
+        colors = ['red', 'blue'] * len(meshes)
+    if opacities is None:
+        opacities = [0.9] * len(meshes)
+
+    # Convert Trimesh objects to Pyvista objects and add them to the plotter
+    for mesh, color, opacity in zip(meshes, colors, opacities):
+        pv_mesh = pv.wrap(mesh)
+        plotter.add_mesh(pv_mesh, color=color, opacity=opacity)
+
+    # Set the title
+    if title is not None:
+        plotter.add_text(title, font_size=20, name='title')
+
+    # Show the plot
+    plotter.show()
+    
 def pre_rotate(mesh, angle):
-    import numpy as np
     """
     Rotate a mesh around the z-axis by a given angle (in degrees).
     """
     T = tra.rotation_matrix(np.radians(angle), [0, 0, 1])
     mesh.apply_transform(T)
 
-def sample_points_in_sphere(mesh, center, radius, num_points):
+def create_cylinder(center, height, radius, num_segments=36):
     """
-    Sample points from the surface of the mesh within a sphere.
+    Create a cylinder mesh centered at the given point.
+
+    Args:
+        center (numpy array): The center of the cylinder [x, y, z].
+        height (float): The height of the cylinder.
+        radius (float): The radius of the cylinder.
+        num_segments (int): The number of segments in the cylinder.
+
+    Returns:
+        A Trimesh object representing the cylinder.
+    """
+    cylinder = trimesh.creation.cylinder(height=height, radius=radius, sections=num_segments)
+    cylinder.vertices += center
+    return cylinder
+
+def sample_points_in_cylinder(mesh, center, height, radius, num_points):
+    """
+    Sample points from the surface of the mesh within a cylinder.
 
     Args:
         mesh (trimesh.Trimesh): The input mesh.
-        center (list or numpy array): The center of the sphere [x, y, z].
-        radius (float): The radius of the sphere.
+        center (list or numpy array): The center of the cylinder [x, y, z].
+        height (float): The height of the cylinder.
+        radius (float): The radius of the cylinder.
         num_points (int): The number of points to sample.
 
     Returns:
@@ -118,54 +131,30 @@ def sample_points_in_sphere(mesh, center, radius, num_points):
     # Get all points on the surface of the mesh
     all_points, _ = trimesh.sample.sample_surface(mesh, num_points * 10)
 
-    # Select points that are within the sphere
-    distances = np.linalg.norm(all_points - center, axis=1)
-    points_in_sphere = all_points[distances <= radius]
+    # Select points that are within the cylinder
+    distances_xy = np.linalg.norm(all_points[:, :2] - center[:2], axis=1)
+    z_differences = np.abs(all_points[:, 2] - center[2])
+    points_in_cylinder = all_points[(distances_xy <= radius) & (z_differences <= height / 2)]
 
     # Randomly select a subset of points if there are more points than needed
-    if points_in_sphere.shape[0] > num_points:
-        indices = np.random.choice(points_in_sphere.shape[0], 
-                                   num_points, replace=False)
-        points_in_sphere = points_in_sphere[indices]
+    if points_in_cylinder.shape[0] > num_points:
+        indices = np.random.choice(points_in_cylinder.shape[0], num_points, replace=False)
+        points_in_cylinder = points_in_cylinder[indices]
 
-    return points_in_sphere
+    return points_in_cylinder
 
-def icp_registration(model1, model2, center, radius, 
-                     sampled=True, samples=10000):
+def icp_registration_with_two_cylinders(model1, model2,
+                                        center1, radius1, height1,
+                                        center2, radius2, height2,
+                                        sampled=True, samples=10000):
     # Align the models using ICP
     if sampled:
-        model1_points = sample_points_in_sphere(model1, center, radius, samples)
-        model2_points = sample_points_in_sphere(model2, center, radius, samples)
-
-        transformation_matrix, _, cost = trimesh.registration.icp(
-            model1_points, model2_points, scale=False
-        )
-    else:
-        transformation_matrix, _, cost = trimesh.registration.icp(
-            model1.vertices, model2.vertices, scale=False
-        )
-
-    # Apply the transformation matrix to model2
-    model2.apply_transform(transformation_matrix)
-
-    return transformation_matrix, cost
-
-def icp_registration_with_two_spheres(model1, model2, 
-                                      center1, radius1, 
-                                      center2, radius2, 
-                                      sampled=True, samples=10000):
-    # Align the models using ICP
-    if sampled:
-        model1_points1 = sample_points_in_sphere(model1, center1, 
-                                                 radius1, samples // 2)
-        model1_points2 = sample_points_in_sphere(model1, center2, 
-                                                 radius2, samples // 2)
+        model1_points1 = sample_points_in_cylinder(model1, center1, radius1, height1, samples // 2)
+        model1_points2 = sample_points_in_cylinder(model1, center2, radius2, height2, samples // 2)
         model1_points = np.vstack((model1_points1, model1_points2))
-        
-        model2_points1 = sample_points_in_sphere(model2, center1, 
-                                                 radius1, samples // 2)
-        model2_points2 = sample_points_in_sphere(model2, center2, 
-                                                 radius2, samples // 2)
+
+        model2_points1 = sample_points_in_cylinder(model2, center1, radius1, height1, samples // 2)
+        model2_points2 = sample_points_in_cylinder(model2, center2, radius2, height2, samples // 2)
         model2_points = np.vstack((model2_points1, model2_points2))
 
         transformation_matrix, _, cost = trimesh.registration.icp(
@@ -180,28 +169,6 @@ def icp_registration_with_two_spheres(model1, model2,
     model2.apply_transform(transformation_matrix)
 
     return transformation_matrix, cost
-
-def plot_meshes(meshes, colors=None, opacities=None, title=None):
-    # Create a Pyvista plotter
-    plotter = pv.Plotter()
-
-    # Set default colors and opacities if not provided
-    if colors is None:
-        colors = ['red', 'blue'] * len(meshes)
-    if opacities is None:
-        opacities = [0.5] * len(meshes)
-
-    # Convert Trimesh objects to Pyvista objects and add them to the plotter
-    for mesh, color, opacity in zip(meshes, colors, opacities):
-        pv_mesh = pv.wrap(mesh)
-        plotter.add_mesh(pv_mesh, color=color, opacity=opacity)
-
-    # Set the title
-    if title is not None:
-        plotter.add_text(title, font_size=20, name='title')
-
-    # Show the plot
-    plotter.show()
 
 def save_stl(mesh, file_path):
     mesh.export(file_path)
@@ -274,33 +241,35 @@ if __name__ == "__main__":
             model1 = load_stl(model1_file_path)
             model2 = load_stl(model2_file_path)
             plot_meshes([model1, model2], colors=['red', 'blue'], 
-                        opacities=[0.5, 0.5],
-                        title="Raw models, post-centering")
-
+                        title="Raw models")
+            
             # pre-rotate model2
-            ROT = -120.0
+            ROT = -30.0
             pre_rotate(model2, ROT)
             plot_meshes([model1, model2], colors=['red', 'blue'], 
                         title=f"Pre-rotated model 2 by {ROT} degrees")
 
             # create sphere to select points of interest and point all three meshes
-            sphere_coord = np.array([-7, 11, 0])
-            radius = 4
-            sphere1 = trimesh.creation.icosphere(subdivisions=3, radius=radius)
-            sphere1.vertices += sphere_coord
-            sphere2 = trimesh.creation.icosphere(subdivisions=3, radius=radius)
-            sphere2.vertices -= sphere_coord
-            plot_meshes([model1, model2, sphere1, sphere2], 
+            # create cylinder to select points of interest and point all three meshes
+            cylinder_center = np.array([6, 21, 0])
+            height = 12
+            radius = 6
+            cylinder1 = create_cylinder(cylinder_center, height, radius)
+            cylinder2 = create_cylinder(-cylinder_center, height, radius)
+            plot_meshes([model1, model2, cylinder1, cylinder2], 
                         colors=['red', 'blue', 'green', 'yellow'], 
-                        title=f"AOI spheres, 1: {sphere_coord}, 2:{-sphere_coord}\nradius: {radius}")
+                        title=(f"AOI cylinders, 1: {cylinder_center}, "
+                               f"2:{-cylinder_center}\nheight: {height}, " 
+                               f"radius: {radius}"))
+
 
             # perform ICP registration
-            """ transformation_matrix, _ = icp_registration(model1, model2, center,
-                                                        radius, sampled=False) """
-            transformation_matrix, _ = icp_registration_with_two_spheres(model1, model2,
-                                                                         sphere_coord, radius,
-                                                                         -sphere_coord, radius,
-                                                                         sampled=False)
+            # transformation_matrix, _ = icp_registration(model1, model2, center,
+            #                                             radius, sampled=False)
+            transformation_matrix, _ = icp_registration_with_two_cylinders(model1, model2,
+                                                                           cylinder_center, radius, height,
+                                                                           -cylinder_center, radius, height,
+                                                                           sampled=True)
             plot_meshes([model1, model2], colors=['red', 'blue'],
                         title="Aligned Models")
             combined_model = trimesh.util.concatenate(model1,
